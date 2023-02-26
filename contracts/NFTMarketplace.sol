afcfaa1d9ef58f0712a277345b6824c29405d321
unknown <kshitijdabral740@gmail.com>
2023-02-26 14:21:03 +0530

    contract updated

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,4 +1,4 @@
-// SPDX-License-identifier: MIT;
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 //INTERNAL IMPORT FOR NFT OPRNZIPLINE
@@ -11,7 +11,29 @@ import "hardhat/console.sol";
 contract NFTMarketplace is ERC721URIStorage{
     using Counters for Counters.Counter;
 
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
 
+    address payable owner;
+    mapping(uint256 => MarketItem) private idMarketItem;
+    struct MarketItem{
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        bool sold; // 
+    }
+
+    event idMarketItemCreated (
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold // 
+    );
+
+    constructor() ERC721("NFT Metaverse Token", "MYNFT"){
+        owner == payable(msg.sender);
+    }
 }
 
 
