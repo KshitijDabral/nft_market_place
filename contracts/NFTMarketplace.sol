349300e3cd508e76681f7f71f01f28dca497ac26
unknown <kshitijdabral740@gmail.com>
2023-02-26 18:28:10 +0530

    contract updated

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -13,7 +13,7 @@ contract NFTMarketplace is ERC721URIStorage{
 
     Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
-
+    uint256 listingPrice = 0.0025 ether;
     address payable owner;
     mapping(uint256 => MarketItem) private idMarketItem;
     struct MarketItem{
@@ -30,10 +30,41 @@ contract NFTMarketplace is ERC721URIStorage{
         uint256 price,
         bool sold // 
     );
-
+    modifier onlyOwner()
+    { require(msg.sender==owner,"only owner of the contract can change the listing price");
+    _;
+    }
     constructor() ERC721("NFT Metaverse Token", "MYNFT"){
         owner == payable(msg.sender);
     }
+    function updateListingPrice(uint256 _ListingPrice) public payable onlyOwner{
+       listingPrice = _ListingPrice ;
+    }
+    function getListingPrice() public view returns(uint256)
+    {
+        return listingPrice;
+    }
+// let create "Create nft oken function"
+
+    function creatToken(string memory tokenURI, uint256 price) public payable returns(uint256){
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        createMarketItem(newTokenId, price);
+
+        return newTokenId;
+    }
+    // creating market items
+    function createMarketItem(uint256 tokenID, uint256 price) private {
+        require(price>0,"price must be atleast 1");
+        require(msg.value== listingPrice, "price must be equal to listing price");
+    }
 }
+
+
+
 
 
